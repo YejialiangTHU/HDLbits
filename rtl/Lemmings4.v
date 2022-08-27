3a914fa3a2d8edafb0e41e534614884b7b34e700
YejialiangTHU <yjl1374141105@163.com>
2022-08-27 21:06:04 +0800

    update code

--- rtl/Lemmings4.v
+++ rtl/Lemmings4.v
@@ -1,15 +1,4 @@
-//In addition to walking and falling, Lemmings can sometimes be told to do useful things, 
-//like dig (it starts digging when dig=1). 
-//A Lemming can dig if it is currently walking on ground (ground=1 and not falling), 
-//and will continue digging until it reaches the other side (ground=0). 
-//At that point, since there is no ground, it will fall (aaah!), 
-//then continue walking in its original direction once it hits ground again. 
-//As with falling, being bumped while digging has no effect, 
-//and being told to dig when falling or when there is no ground is ignored.
-//(In other words, a walking Lemming can fall, dig, or switch directions. 
-//If more than one of these conditions are satisfied, fall has higher precedence than dig, 
-//which has higher precedence than switching directions.)
-module Lemmings3(
+module Lemmings4(
     input       clk,
     input       areset,    // Freshly brainwashed Lemmings walk left.
     input       bump_left,
@@ -19,25 +8,26 @@ module Lemmings3(
     output      walk_left,
     output      walk_right,
     output      aaah,
-    output      digging
+    output      digging    
     ); 
 
-    parameter LEFT=3'b000;
-    parameter RIGHT=3'b001;
-    parameter FALL_L=3'b010;
-    parameter FALL_R=3'b011;
-    parameter DIG_L = 3'b100;
-    parameter DIG_R = 3'b101;   
+    parameter LEFT      =   3'b000;
+    parameter RIGHT     =   3'b001;
+    parameter FALL_L    =   3'b010;
+    parameter FALL_R    =   3'b011;
+    parameter DIG_L     =   3'b100;
+    parameter DIG_R     =   3'b101;
+    parameter SPLAT     =   3'b110;   
     
     reg		[2:0]   cur_state, nxt_state;
     wire	[1:0]	bump_lr;
-
+    reg     [9:0]   fall_count;
+    
     assign bump_lr = {bump_left, bump_right};
     
     always @ (*) begin
         case (cur_state)
             default: nxt_state = LEFT;
-
             LEFT: 	if (ground==1'b0) begin
                 		nxt_state = FALL_L;
            	 		end else if(ground == 1'b1) begin
@@ -48,13 +38,13 @@ module Lemmings3(
                         end else begin
                           	nxt_state = LEFT;
                         end
-                   end else begin
+                    end else begin
                    		nxt_state = LEFT;
-                   end
+                    end
             
             RIGHT:  if (ground==1'b0)
                 		nxt_state = FALL_R;
-                        else if (ground == 1'b1)
+                    else if (ground == 1'b1)
                 		if (dig == 1'b1)
                     		nxt_state = DIG_R;
             			else if (bump_lr==2'b01 || bump_lr==2'b11)
@@ -64,25 +54,30 @@ module Lemmings3(
             		else
                           	nxt_state = RIGHT;
             
-            FALL_L: if (ground == 1'b1)
+            FALL_L: if (ground == 1'b1 && fall_count<=10'd19)
                 		nxt_state = LEFT;
-            		else 
-                     	nxt_state = FALL_L;
+            		else if (ground ==1'b1 && fall_count>10'd19)
+                     	nxt_state = SPLAT;
+                    else
+                        nxt_state = FALL_L;
             
-            DIG_L: if (ground == 1'b0)
+            DIG_L:  if (ground == 1'b0)
                 		nxt_state = FALL_L;
             		else 
                      	nxt_state = DIG_L;            
             
-            FALL_R: if (ground == 1'b1)
+            FALL_R: if (ground == 1'b1 && fall_count<=10'd19)
                 		nxt_state = RIGHT;
+                    else if (ground ==1'b1 && fall_count>10'd19)
+                        nxt_state = SPLAT;
             		else 
                      	nxt_state = FALL_R;
             
-            DIG_R: if (ground == 1'b0)
+            DIG_R:  if (ground == 1'b0)
                 		nxt_state = FALL_R;
             		else 
-                     	nxt_state = DIG_R;            
+                     	nxt_state = DIG_R;
+            SPLAT:  nxt_state = SPLAT; 
         endcase
     end
     
@@ -93,12 +88,25 @@ module Lemmings3(
             cur_state <= nxt_state;
         end
     end
+    
+   //fall count
+
+    always @ (posedge clk or posedge areset) begin
+        if (areset) begin
+            fall_count <= 10'd0;
+        end else if ((cur_state == FALL_L || cur_state == FALL_R)) begin
+            if (ground==1'b1)
+            	fall_count <= 10'd0;
+            else
+            	fall_count <= fall_count + 1'b1;
+        end 
+    end
    
     //output logic
     
     assign walk_left = cur_state== LEFT;
     assign walk_right = cur_state== RIGHT;
-    assign digging = (ur_state== DIG_L || cur_state== DIG_R);
+    assign digging = (cur_state== DIG_L || cur_state== DIG_R);
     assign aaah = (cur_state==FALL_L || cur_state==FALL_R); 
-     
+    
 endmodule
